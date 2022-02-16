a5f37e864e92e1816f3b62e7418a98fd733f1ce4
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-02-16 23:18:06 +0300

    init commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import * as d3 from "d3"
+import { fun } from "./fun"
+
+document.addEventListener("DOMContentLoaded", function () {
+	console.log(d3)
+
+	let a = 4
+	console.log(a)
+
+	fun()
+})
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path")
+
+module.exports = {
+	entry: "./src/index.js",
+	mode: "production",
+	module: {},
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "build"),
+	},
+	resolve: {
+		extensions: [".js"],
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "build"),
+		},
+		port: 8080,
+		open: true,
+	},
+}
