288d970a867efe2bf52a100bd59c1adddb89ccfb
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-03-27 00:59:58 +0400

    ts config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
 const path = require("path")
 
+const buildDirectory = path.resolve(__dirname, "build")
+
 module.exports = {
 	entry: "./src/index.js",
 	mode: "development",
-	module: {},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
 	output: {
 		filename: "bundle.js",
-		path: path.resolve(__dirname, "build"),
+		path: buildDirectory,
 	},
 	resolve: {
 		extensions: [".js"],
 	},
 	devServer: {
 		static: {
-			directory: path.join(__dirname, "build"),
+			directory: buildDirectory,
 		},
 		port: 8080,
 		open: true,
